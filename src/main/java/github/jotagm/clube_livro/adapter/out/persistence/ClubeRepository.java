2596a5e08d8c61bbb46715e81f42c7eb329edc00
jotagm <devjotagm@gmail.com>
2026-07-06 16:57:44 -0300

    fix: chamadas de clube na home page

--- src/main/java/github/jotagm/clube_livro/adapter/out/persistence/ClubeRepository.java
+++ src/main/java/github/jotagm/clube_livro/adapter/out/persistence/ClubeRepository.java
@@ -3,6 +3,7 @@ package github.jotagm.clube_livro.adapter.out.persistence;
 import github.jotagm.clube_livro.domain.clube.Clube;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
@@ -10,7 +11,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ClubeRepository extends JpaRepository <Clube, UUID>{
+
+    @EntityGraph(attributePaths = "temas")
+    Optional<Clube> findById(UUID id);
+
+    @EntityGraph(attributePaths = "temas")
     Optional<Clube> findByNome(String nome);
 
+    @EntityGraph(attributePaths = "temas")
     Page<Clube> findByPrivadoFalseOrIdIn(Collection<UUID> ids, Pageable pageable);
 }
